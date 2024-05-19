0da52e13bd2a2b1c9fb26cd56919374aab5431ae
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-05-19 22:54:23 +0200

    chore(META): add new options buildArg

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
 config="${INPUT_CONFIG:-fly.toml}"
-buildArgs=(echo "$INPUT_BUILDARG" | sed 's/\([^ ]*\)/--build-arg \1/g')
+buildArgs=$(echo "$INPUT_BUILDARG" | sed 's/\([^ ]*\)/--build-arg \1/g')
 
 if ! echo "$app" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
