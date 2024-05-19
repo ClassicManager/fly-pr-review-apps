ccf0fca4c7a01003d76db18378b8d169518d63b7
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-05-19 23:04:39 +0200

    chore(META): add new options buildArg

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,6 @@ region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
 config="${INPUT_CONFIG:-fly.toml}"
-buildArgs=$(echo "$INPUT_BUILDARG" | sed 's/[^ ]\+/"--build-arg &"/g' | tr -d '\n' | sed 's/--build-arg/ --build-arg/g')
 
 if ! echo "$app" | grep "$PR_NUMBER"; then
   echo "For safety, this action requires the app's name to contain the PR number."
@@ -59,7 +58,7 @@ fi
 # Trigger the deploy of the new version.
 echo "Contents of config $config file: " && cat "$config"
 if [ -n "$INPUT_VM" ]; then
-  flyctl deploy "$INPUT_BUILDARG" --build-target "$INPUT_BUILDTARGET" --dockerfile "$INPUT_DOCKERFILE" --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
+  flyctl deploy --build-arg "FILTER=--filter=+@classicmanager/$INPUT_BUILDARG" --build-target "$INPUT_BUILDTARGET" --dockerfile "$INPUT_DOCKERFILE" --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
 else
   flyctl deploy "$INPUT_BUILDARG" --build-target "$INPUT_BUILDTARGET" --dockerfile "$INPUT_DOCKERFILE" --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY"
 fi
